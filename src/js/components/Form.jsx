af91177ebbf74429d3359cc25d4e42ad813adc71
Mark Hsu <mark.g.hsu@gmail.com>
2019-04-29 20:42:58 -0400

    Add Form - use mapDispatchToProps to map actions into react return an object which contains all the props, along with a function that handles the actual dispatch of the specific action

---
+++ src/js/components/Form.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addArticle } from '../actions/index'
+
+function mapDispatchToProps(dispatch) {
+	//need to return an object
+	//where every property is the name of a prop in our component
+	//since this prop is a function
+	//we return a function that calls DISPATCH (which will be passed from `connect`)
+	//along with the action (created using our actionCreator from the action js folder)
+	return {
+		addArticle: article => dispatch(addArticle(article))
+	};
+}
+
+class ConnectedForm extends Component {
+	constructor() {
+		super();
+		this.num = 0;
+		this.state = {
+			title: ''
+		};
+
+		//bind our `this` so we can use the handleChange and handleSubmit
+		//functions to handle our changing and submitting
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	nextNumber(){
+		this.num++;
+		return this.num;
+	}
+	handleChange(event) {
+		//our state contains all of the fields of the form
+		//each field is it's own property, with the value of the field as the value
+		this.setState({ [event.target.id]:event.target.value });
+	}
+
+	handleSubmit(event){
+		event.preventDefault();//DON'T ALLOw ACTUAL EVENT SUBMISSION
+		const { title } = this.state;
+		const id = this.nextNumber();
+		//THE ACTION WILL BE PASSED TO US AS A PROP, FROM mapDispatchToProps
+		this.props.addArticle({title, id});
+		this.setState({title: ''}); //reset our state to empty
+		//@TODO: can we auto clear all fields?
+	}
+
+	render() {
+		const { title } = this.state;
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<div className="form-group">
+					<label htmlFor="title">Title</label>
+					<input 
+						type="text"
+						className="form-control"
+						id="title"
+						value={title}
+						onChange={this.handleChange}
+					/>
+				</div>
+				<button type="submit" className="btn btn-success btn-lg">
+					SAVE
+				</button>
+			</form>
+		);
+	}
+}
+
+const Form = connect(null,mapDispatchToProps)(ConnectedForm);
+
+export default Form;
